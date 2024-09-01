c5dcf16b54ea619e90a4a3225888e85261813aff
joaopaulo-oss <joaopaulo24973@gmail.com>
2024-09-01 04:17:29 -0300

    o preço do produto e calcule o preço final com o imposto incluido

---
+++ atv07.py
@@ -0,0 +1,9 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído.
+
+#receber o valor do produto
+valorproduto = float(input("digite o valor do produto"))
+
+#calcular o valor do importo em relacao ao valor do produto
+valorimposto = valorProduto * 12/100
+
+#soma
